c23c648e151bd511d7a88b2a21eca6d4798c4e3d
Nirajanlama201 <lamanirajan502@gmail.com>
2026-03-30 22:29:35 +0545

    Node class

--- app.js
+++ app.js
@@ -1,9 +1,22 @@
-const amount = 8
+// CommonJS, every file is module (by default)
+// Modules - Encapsulated Code (only share minimum)
 
-if (amount < 10){
-    console.log('small number')
-}else{
-    console.log('large number')
-}
+const john = 'john'
+const peter = 'peter'
 
-console.log(`hey it's my first node app!!!`)
+ const sayHi =(name)=>{
+    console.log(`hello there ${name}`)
+ }
+
+ sayHi('susan')
+ sayHi(john)
+ sayHi(peter)
+
+
+// const names = require('./04-names')
+// const sayHi = require('./05-utils')
+// const data = require('./06-alternative-flavor')
+// require('./07-mind-grenade')
+// sayHi('susan')
+// sayHi(names.john)
+// sayHi(names.peter)
